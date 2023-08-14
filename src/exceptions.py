221f5ac6975154bba4b07857c08f8b6d9848613e
Mike Lynch <m.lynch@sydney.edu.au>
2023-08-14 14:08:53 +1000

    Root commit

---
+++ src/exceptions.py
@@ -0,0 +1,17 @@
+
+
+class ROCrateException(Exception):
+	pass
+
+
+class ROCrateJSONException(ROCrateException):
+    pass
+
+
+class ROCrateRootDataEntityException(ROCrateException):
+    pass
+
+
+class ROCrateContentsException(ROCrateException):
+    pass
+
